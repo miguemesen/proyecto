97b54a21619e00c5213f964edf4d1119d8438864
miguemesen <miguemesen12@gmail.com>
2024-06-21 02:37:27 -0600

    update example of api working

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const fetchTestApi = async () => {
     const res = await test();
     const resData = await res.json();
-    setTest(resData)
+    setTest(resData.test)
   }
 
   useEffect(() => {
